a2af80bb8a0baae2b3c4254ae5bc76275919dcb0
戴季鸿 <daijihong@mindflow.com.cn>
2024-01-02 10:30:55 +0800

    small change

---
+++ test.py
@@ -0,0 +1,30 @@
+from torchvision.transforms import (
+    Resize,CenterCrop,ToTensor,ToPILImage,InterpolationMode,Compose,Lambda
+)
+from PIL import Image
+import torch
+from torch import nn
+
+in_channels=None
+out_channels=None
+
+def img2tensor_module():
+    size=512
+    return Compose([
+        Resize(size),
+        CenterCrop(size),
+        ToTensor(),
+        Lambda(lambda t:(t*2)-1)
+    ])
+
+conv=nn.Conv2d(
+    in_channels,out_channels,
+    kernel_size=3,stride=2,padding=1
+)
+maxPool=nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
+norm=nn.GroupNorm(num_groups=3,num_channels=3,eps=1e-4)
+
+img=Image.open("images/ling.jpg")
+toTensor=img2tensor_module()
+ten=toTensor(img)
+print(ten.shape)
